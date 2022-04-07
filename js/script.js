25abbf7ffd5c98ceffabf137e9e1a9600d2ea11d
krista-t <krista.tomicic@gmail.com>
2022-04-07 14:54:48 +0200

    img front end console error fixed

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
-
 //make sure if cookie is present, UI displays correctly
 
 window.addEventListener("load", checkCookieExists)
-function checkCookieExists(){
-  if(document.cookie){
+
+function checkCookieExists() {
+  if (document.cookie) {
     console.log("true, cookie here")
     document.querySelector("#login-btn").classList.add("hidden")
   }
 }
 
 let stateObj = {
-home: "/"
+  home: "/"
 }
 
 //fetch tweets
 async function tweet() {
-    const form = event.target.form
-     const connection = await fetch("/tweet", {
-       method: "POST",
-       body: new FormData(form)
-     })
-     console.log(connection)
-     if (!connection.ok) {
-       alert("Could not tweet")
-       return
-     }
-     // Success
-     let tweet = await connection.json()
-      console.log(tweet)
+  const form = event.target.form
+  const connection = await fetch("/tweet", {
+    method: "POST",
+    body: new FormData(form)
+  })
+  console.log(connection)
+  if (!connection.ok) {
+    alert("Could not tweet")
+    return
+  }
+  // Success
+  let tweet = await connection.json()
+  console.log(tweet)
+  const tweet_form = document.querySelector("#tweet-form")
+  const tweet_img = document.querySelector("#upload")
+  const tweet_text = document.querySelector("#tweet_text")
 
-     //INSERT  <img class="mt-2 w-full object-cover h-20" src=${tweet.image}> INTO GAP DOWN
-     let section_tweet = `
+  //INSERT  <img class="mt-2 w-full object-cover h-20" src=${tweet.image}> INTO GAP DOWN
+  let section_tweet = `
           <div class="p-4 border-t border-slate-200">
           <div class="flex">
             <img class="flex-none w-12 h-12 rounded-full" src="" alt="photo">
@@ -44,7 +47,7 @@ async function tweet() {
               <div class="pt-2">
               ${tweet.text}
               </div>
-              <img class="mt-2 w-full object-cover h-22" src="/img/${tweet.src}">
+              <img id = "tweet-img" class="mt-2 w-full object-cover h-22" src="/img/${tweet.src}">
               <div class="flex gap-12 w-full mt-4 text-lg">
                   <i class="fa-solid fa-message ml-auto"></i>
                   <i class="fa-solid fa-heart"></i>
@@ -54,69 +57,72 @@ async function tweet() {
             </div>
           </div>
         </div>`
-     document.querySelector("#fweets").insertAdjacentHTML("afterbegin", section_tweet)
-     console.log(document.querySelector("#tweet-form").value)
+  document.querySelector("#fweets").insertAdjacentHTML("afterbegin", section_tweet)
+  if (tweet.src == ""){
+    console.log("EMPTY")
+    document.querySelector("#tweet-img").src = ""
+  }
+  tweet_form.reset()
 
-   }
+}
 
-   //////////
-   //fetch users
-   async function createUser() {
-      const form = event.target.form;
-      const connection = await fetch("/signup", {
-        method: "POST",
-        body: new FormData(form)
+//////////
+//fetch users
+async function createUser() {
+  const form = event.target.form;
+  const connection = await fetch("/signup", {
+    method: "POST",
+    body: new FormData(form)
 
-      })
-      let userData = await connection.json()
-      document.querySelector("#server-error-msg").innerHTML = userData["msg"]
-      document.querySelector("#server-error-msg").style.color = "red"
-      console.log(userData)
-      if (!connection.ok) {
-        console.log("Cannot sign up")
+  })
+  let userData = await connection.json()
+  document.querySelector("#server-error-msg").innerHTML = userData["msg"]
+  document.querySelector("#server-error-msg").style.color = "red"
+  console.log(userData)
+  if (!connection.ok) {
+    console.log("Cannot sign up")
 
-      }else{
-        //check is all validations are successfull and redirect to homepage
-        if (userData.success) {
-          console.log("success")
-          window.location = "/"
-        }
-      }
-     }
+  } else {
+    //check is all validations are successfull and redirect to homepage
+    if (userData.success) {
+      console.log("success")
+      window.location = "/"
+    }
+  }
+}
 
-     function showLogInForm(){
-       document.querySelector("#login").classList.remove("hidden")
-      history.pushState(stateObj, "login", "login")
-      console.log(stateObj)
-     }
+function showLogInForm() {
+  document.querySelector("#login").classList.remove("hidden")
+  history.pushState(stateObj, "login", "login")
+  console.log(stateObj)
+}
 
-       //fetch loggedin
-       async function logUser(){
-         const form = event.target.form
-          const connection = await fetch("/login", {
-            method: "POST",
-            body: new FormData(form)
-          })
-          let loggedUserValidation = await connection.json()
-          document.querySelector("#server-error-msg").innerHTML = loggedUserValidation["msg"]
-          document.querySelector("#server-error-msg").style.color = "red"
-          console.log(loggedUserValidation)
-          console.log(connection)
-          if (!connection.ok) {
-            console.log("Cannot sign up")
-            }
+//fetch loggedin
+async function logUser() {
+  const form = event.target.form
+  const connection = await fetch("/login", {
+    method: "POST",
+    body: new FormData(form)
+  })
+  let loggedUserValidation = await connection.json()
+  document.querySelector("#server-error-msg").innerHTML = loggedUserValidation["msg"]
+  document.querySelector("#server-error-msg").style.color = "red"
+  console.log(loggedUserValidation)
+  console.log(connection)
+  if (!connection.ok) {
+    console.log("Cannot sign up")
+  }
 
-             if (loggedUserValidation.msg === "User does not exist!") {
-               window.location = "/signup"
-             }
-           else{
-             //check is all validations are successfull, hide login popup, and show logout btn
-             if (loggedUserValidation.success) {
-               console.log("success")
-               document.querySelector("#login").classList.add("hidden")
-               document.querySelector("#login-btn").classList.add("hidden")
-               history.pushState(stateObj, "/", "tweets")
+  if (loggedUserValidation.msg === "User does not exist!") {
+    window.location = "/signup"
+  } else {
+    //check is all validations are successfull, hide login popup, and show logout btn
+    if (loggedUserValidation.success) {
+      console.log("success")
+      document.querySelector("#login").classList.add("hidden")
+      document.querySelector("#login-btn").classList.add("hidden")
+      history.pushState(stateObj, "/", "tweets")
 
-             }
-           }
-         }
+    }
+  }
+}
--- post_tweet.py
+++ post_tweet.py
@@ -14,12 +14,10 @@ def validate_img(image):
         image_id = str(uuid.uuid4())
         # Create new image name
         tweet_img = f"{image_id}{file_extension}"
-        print("#########", tweet_img)  # 23cf4153-c03a-4fe1-b2ca-7fbb9b973798.png
+        print("#########", tweet_img)
         # Save the image
         image.save(f"img/{tweet_img}")
-        #print("AAAAAAA", f"img/{tweet_img}")
         imghdr_extension = imghdr.what(f"img/{tweet_img}")
-        #print(f".{imghdr_extension}")  # .jpeg
         if file_extension != f".{imghdr_extension}":
             print(globals.ERROR["error_img"])
             os.remove(f"img/{tweet_img}")
@@ -28,7 +26,9 @@ def validate_img(image):
             return tweet_img
     #check if img exists
     elif not image:
-        return image == ""
+        print("NO IMAGE")
+        tweet_img = ""
+        return tweet_img #None
 
 ##############################
 @post("/tweet")
