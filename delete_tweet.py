0b3c7772ac8ac00091ee60f2633fd701d1c302db
krista-t <krista.tomicic@gmail.com>
2022-04-22 21:20:29 +0200

    commit

--- delete_tweet.py
+++ delete_tweet.py
@@ -1,22 +1,22 @@
-from bottle import  delete, response
+from bottle import delete
 import sqlite3
 import globals
 
 ##############################
 @delete("/delete_tweet/<tweet_id>")
 def _(tweet_id):
-   print(tweet_id)
     #validate that the tweet_id is a valid UUID4
-   if globals._is_uuid4(tweet_id):
-    db = sqlite3.connect("database.sqlite")
+    if globals._is_uuid4(tweet_id):
+        db = sqlite3.connect("database.sqlite")
     try:
-        db.execute(globals.DELETE_TWEET_QUERY, (tweet_id,)).fetchone()
+        deleted = db.execute(globals.DELETE_TWEET_QUERY, (tweet_id,)).fetchone()
         db.commit()
     except Exception as ex:
         print(ex)
+        ex = globals._send(500, "something went wrong")
+        return ex
     finally:
         db.close()
-        return "deleted"
-
+        return deleted
 
 
