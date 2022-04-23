dbed813021ae68c8cb5c2ad673c3de54680f6c96
krista-t <krista.tomicic@gmail.com>
2022-04-23 14:13:52 +0200

    loggeduser cannot interact with different single profile

--- js/profile.js
+++ js/profile.js
@@ -1,20 +1,38 @@
- //update tweet
+
+//check who is logged in
+function checkProfile(){
+    let loggedUser = document.querySelector("#logged-user span").textContent
+    loggedUser = loggedUser.substring(1)
+  let profileUser = document.querySelector("#user-info p").id
+    if(loggedUser != profileUser){
+        console.log("NOT")
+        document.querySelector("#profile-form button").disabled = true
+        document.querySelector("#profile-form input").disabled = true
+    }else{
+        console.log("BOT")
+    }
+}
+checkProfile()
+
+
+ //update profile img
  async function uploadProfilePic(id) {
     const form = event.target.form
-    console.log("form")
     //Connect to the api
     const connection = await fetch(`/${id}`, {
       method: "PUT",
       body: new FormData(form)
     })
+
     console.log(connection)
     if (!connection.ok) {
       alert("Wrong image format, only .jpg, .jpeg, and png allowed")
       return
     }
     //Success
+    let profileImg =  document.querySelector("#profile-photo")
     let updatedImg = await connection.json()
-    document.querySelector("#profile-photo").src = "/img/" + updatedImg.user_image + ""
+    profileImg.src = "/img/" + updatedImg.user_image + ""
     document.querySelector("#main-img").src = "/img/" + updatedImg.user_image + ""
     document.querySelector("#left-panel-img").src = "/img/" + updatedImg.user_image + ""
   }
