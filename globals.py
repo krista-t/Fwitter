061d2ece2b20d8dae4fef3e49ff5d5c3052d1224
krista-t <krista.tomicic@gmail.com>
2023-07-31 12:51:35 +0200

    some cleanup

--- globals.py
+++ globals.py
@@ -6,35 +6,40 @@ import imghdr
 import os
 
 TRENDS = [
-  {"category": "Technology", "title": "github", "tweets_counter": "53K"},
-  {"category": "Arts&culture", "title": "#photography", "tweets_counter": "40k"},
-  {"category": "Politics . Trending", "title": "Ukraine", "tweets_counter": "101M"},
-  {"category": "Trends", "title": "#caturday", "tweets_counter": "19k"},
-  {"category": "Trending in DK", "title": "Roskilde Festival", "tweets_counter": "45k"},
-  {"category": "Music", "title": "Spotify", "tweets_counter": "45k"},
+    {"category": "Technology", "title": "github", "tweets_counter": "53K"},
+    {"category": "Arts&culture", "title": "#photography", "tweets_counter": "40k"},
+    {"category": "Politics . Trending", "title": "Ukraine", "tweets_counter": "101M"},
+    {"category": "Trends", "title": "#caturday", "tweets_counter": "19k"},
+    {"category": "Trending in DK", "title": "Barbie", "tweets_counter": "95k"},
+    {"category": "Music", "title": "Spotify", "tweets_counter": "45k"},
 ]
 
 REGEX_EMAIL = '^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
 
-##############################
-def _is_uuid4(text=None):
-  if not text: return None
-  regex_uuid4 = "^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
-  if not re.match(regex_uuid4, text) : return None
-  print(f"{text} valid uuid")
-  return text
 
-##############################
+# valdate uuid
+def _is_uuid4(text=None):
+    if not text:
+        return None
+    regex_uuid4 = (
+        "^[0-9a-f]{8}-[0-9a-f]{4}-[4][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
+    )
+    if not re.match(regex_uuid4, text):
+        return None
+    print(f"{text} valid uuid")
+    return text
+
+
+# validate img
 def validate_img(image):
-     #validate img format
     if image:
         file_name, file_extension = os.path.splitext(image.filename)
         if file_extension not in (".png", ".jpeg", ".jpg"):
-         print("image not allowed")
+            print("image not allowed")
         image_id = str(uuid.uuid4())
-        # Create new image name
+        # create new image name
         img = f"{image_id}{file_extension}"
-        # Save the image
+        # save the image
         image.save(f"img/{img}")
         imghdr_extension = imghdr.what(f"img/{img}")
         if file_extension != f".{imghdr_extension}":
@@ -43,13 +48,13 @@ def validate_img(image):
             return globals.ERROR["error_img"]
         else:
             return img
-    #check if img exists
+    # check if img exists
     elif not image:
         print("NO IMAGE")
         img = ""
         return img
 
-##############################
+
 # create json from sqliteDB
 def create_json_from_sqlite_result(cursor, row):
     d = {}
@@ -57,14 +62,14 @@ def create_json_from_sqlite_result(cursor, row):
         d[col[0]] = row[idx]
     return d
 
-##############################
+
 # connect database
 def _db_connect(db_name):
     db = sqlite3.connect(db_name)
     db.row_factory = create_json_from_sqlite_result
     return db
 
-##############################
+
 # error messages
 ERROR = {
     "error_name_min": "name at least 2 characters",
@@ -75,52 +80,50 @@ ERROR = {
     "error_password_min": "password must be at least 6 characters",
     "error_password": "enter password",
     "error_img": "Wrong image format, only png, jpg, jpeg allowed",
-    "error_tweet_text": "must be 80 characters or less"
+    "error_tweet_text": "must be 80 characters or less",
 }
 
-##############################
-def _send(status = 400, error_message = "unknown error"):
-  response.status = status
-  print(status)
-  return {status:error_message}
 
-##############################
+def _send(status=400, error_message="unknown error"):
+    response.status = status
+    print(status)
+    return {status: error_message}
+
+
 ##DB QUERIES##
-#singup queries
+# singup queries
 USER_NAME_QUERY = """
 SELECT user_name FROM users where user_name = ?
 """
 USER_EMAIL_QUERY = """
 SELECT user_email FROM users where user_email = ?
 """
-#login query
+# login query
 USER_NAME_PASS_IMG_QUERY = """
 SELECT user_name, user_password, user_image FROM users where user_name = ?
 """
-#delete session query
+# delete session query
 DELETE_SESS_ROW_QUERY = """
 DELETE FROM sessions WHERE session_id = ?"""
 
-#delete tweet query
+# delete tweet query
 DELETE_TWEET_QUERY = """
 DELETE FROM tweets WHERE tweet_id = ?"""
 
-#show tweet with specific id query
+# show tweet with specific id query
 GET_TWEET_WITH_ID_QUERY = """
 SELECT * FROM tweets WHERE tweets.tweet_id = ?
 """
-#get profile
+# get profile
 GET_USER_QUERY = """
 SELECT user_id, user_name, user_full_name, user_image, user_created_at FROM users WHERE users.user_name = ?
 """
-#get single user tweet
+# get single user tweet
 GET_USER_TWEET_QUERY = """
 SELECT * FROM tweets WHERE tweets.user_id = ? ORDER by tweet_created_at DESC
 """
 
-#get logged user img on the left panel
+# get logged user img on the left panel
 GET_LOGGED_USER_IMG_QUERY = """
 SELECT * FROM users WHERE users.user_name = ?
 """
-
-##############################
